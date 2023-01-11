b59a803fb421fc1ed2466eeba2e444ec85c7ba5b
lawrenceliao <lawrenceltq@gmail.com>
2023-01-12 02:22:32 +1100

    updated-basic-configuration

--- src/main/java/co/zip/candidate/userapi/UserApiApplication.java
+++ src/main/java/co/zip/candidate/userapi/UserApiApplication.java
@@ -3,11 +3,19 @@ package co.zip.candidate.userapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class UserApiApplication {
 
+	private static final String TIME_ZONE = "UTC";
 	public static void main(String[] args) {
 		SpringApplication.run(UserApiApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone(TIME_ZONE));
+	}
 }
