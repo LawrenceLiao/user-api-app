453c24b5810dd64060abb8a22df73f52ac2703e4
lawrenceliao <lawrenceltq@gmail.com>
2023-01-12 02:41:38 +1100

    created-entities

---
+++ src/main/java/co/zip/candidate/userapi/model/Account.java
@@ -0,0 +1,65 @@
+package co.zip.candidate.userapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.Currency;
+
+import static co.zip.candidate.userapi.constant.CurrencyInstance.AUD;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false)
+    private Long id;
+
+    @Column(unique = true, updatable = false, nullable = false)
+    private String accountNum;
+
+    @Builder.Default
+    @Column(updatable = false, nullable = false)
+    private Currency currency = AUD;
+
+    @Column(nullable = false)
+    private BigDecimal totalCredit;
+
+    @Column(nullable = false)
+    private BigDecimal availableCredit;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private OffsetDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+}
---
+++ src/main/java/co/zip/candidate/userapi/model/Client.java
@@ -0,0 +1,67 @@
+package co.zip.candidate.userapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.List;
+
+import static co.zip.candidate.userapi.constant.CurrencyInstance.AUD;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Currency currency = AUD;
+    @Column(nullable = false)
+    private BigDecimal monthlySalary;
+
+    @Column(nullable = false)
+    private BigDecimal monthlyExpenses;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private OffsetDateTime updatedAt;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "client", fetch = FetchType.LAZY)
+    private List<Account> accounts = new ArrayList<>();
+}
