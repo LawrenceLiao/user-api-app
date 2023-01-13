2050caf23357e68421cd2639c61b6283fd27636e
lawrenceliao <lawrenceltq@gmail.com>
2023-01-13 13:05:05 +1100

    replaced-single-map

--- src/main/java/co/zip/candidate/userapi/dictionary/ThresholdDic.java
+++ src/main/java/co/zip/candidate/userapi/dictionary/ThresholdDic.java
@@ -3,17 +3,19 @@ package co.zip.candidate.userapi.dictionary;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.util.Collections;
 import java.util.Currency;
+import java.util.HashMap;
 import java.util.Map;
 
 import static co.zip.candidate.userapi.constant.CurrencyInstance.AUD;
 
 @Component
 public class ThresholdDic {
-    private final Map<Currency, BigDecimal> thresholdMap = Collections.singletonMap(
-            AUD, BigDecimal.valueOf(1000)
-    );
+    private final Map<Currency, BigDecimal> thresholdMap = new HashMap<Currency, BigDecimal>(){
+        {
+            put(AUD, BigDecimal.valueOf(1000));
+        }
+    };
 
     public BigDecimal getCurrencyThreshold(Currency currency) {
         return thresholdMap.get(currency);
